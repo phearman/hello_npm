9b743c8d0bb15c561979e94287d0e9040b772a4b
i24589 <along78109@icsc.com.tw>
2018-05-09 13:45:57 +0800

    push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
+var moment = require('moment');
 var zzz = require('./zzz');
 console.log(zzz.cube(3)); // 27
 console.log(zzz.foo);    // 4.555806215962888
-// console.log(zzz.zzz);   
+console.log(zzz.zzz);   
+console.log('hi....',moment().format('YYYY.MM.DD HH:mm'));
