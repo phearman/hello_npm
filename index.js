7ba656c95d021656c3fb1e6aa43b704119c3e83a
i24589 <along78109@icsc.com.tw>
2018-05-09 14:11:19 +0800

    commit

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log(zzz.cube(3)); // 27
 console.log(zzz.foo);    // 4.555806215962888
 console.log(zzz.zzz);   
 console.log('hi....',moment().format('YYYY.MM.DD HH:mm'));
+console.error(123);
